d319fed42ea4ded01c24af4eada923b3cc4989cf
SwaroopDangal <swaroopdangal732@gmail.com>
2026-02-10 18:08:39 +0545

    feat: add ChatBox component for real-time messaging and integrate with App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Messages from "./pages/Messages";
 import MyOrders from "./pages/MyOrders";
 import Loading from "./pages/Loading";
 import Navbar from "./components/Navbar";
+import ChatBox from "./components/ChatBox";
 
 const App = () => {
   const { pathname } = useLocation();
@@ -27,6 +28,7 @@ const App = () => {
         <Route path="/my-orders" element={<MyOrders />} />
         <Route path="/loading" element={<Loading />} />
       </Routes>
+      <ChatBox />
     </div>
   );
 };
