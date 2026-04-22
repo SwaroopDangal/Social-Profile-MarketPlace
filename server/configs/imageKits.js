b30676613b1471dc2068d1fa66ef7c8d7b61aad8
SwaroopDangal <swaroopdangal732@gmail.com>
2026-04-22 17:17:49 +0545

    feat: add .gitignore and configure dotenv for environment variables

--- server/configs/imageKits.js
+++ server/configs/imageKits.js
@@ -1,7 +1,10 @@
 import ImageKit from '@imagekit/nodejs';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const imageKit = new ImageKit({
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
 });
 
 export default imageKit;
